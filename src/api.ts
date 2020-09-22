ec1a809376b8885f668d8afcd2d4873addd5fab1
jeison.azevedo <jeison.azevedo@softplan.com.br>
2020-09-22 18:13:42 -0300

    remover server

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:"+process.env.PORT
+    baseURL: "http://localhost:" + process.env.PORT ?? 3333
 })
 
 export default api
